a6ee747b987f67997192d8b96ddd6ff299705ee8
nicollyADS <rm552579@fiap.com.br>
2023-09-19 15:18:30 -0300

    criando o componente AdicionarProdutos e imports

---
+++ src/routes/AdicionarProdutos.jsx
@@ -0,0 +1,11 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { ListaProdutos } from "../components/ListaProdutos";
+import { useState } from "react";
+
+export default function AdicionarProdutos (){
+	return (
+		<>
+		</>
+	)
+					
+}
