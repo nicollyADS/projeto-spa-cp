0413cc3d3629033792b476e6ce0ffbb6c0ae7946
nicollyADS <rm552579@fiap.com.br>
2023-09-19 15:23:17 -0300

    criando o html

--- src/routes/AdicionarProdutos.jsx
+++ src/routes/AdicionarProdutos.jsx
@@ -5,6 +5,31 @@ import { useState } from "react";
 export default function AdicionarProdutos (){
 	return (
 		<>
+			<div>
+            <h1>Adicionar Produto</h1>
+            <div>
+                <form>
+                    <fieldset>
+                            <legend>Informações do Produto</legend>
+                            <input type="hidden" name="id"/>
+                        <div>
+                            <label htmlFor="idProduto">Nome do produto</label>
+                            <input type="text" name = "nome" id="idProd"/>
+                        </div>
+                        <div>
+                            <label htmlFor="idDesc">Descrição</label>
+                            <input type="text" name = "desc" id="idDesc"/>
+                        </div>
+                        <div>
+                            <label htmlFor="idPreco">Preço do produto</label>
+                            <input type="text" name = "preco" id="idPreco"/>
+                        </div>
+                        <button>Adicionar</button>
+                        <button>Cancelar</button>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
 		</>
 	)
 					
